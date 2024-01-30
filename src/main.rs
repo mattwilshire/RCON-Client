8f81020b88ada0726e8968fcc97525cc271a1a68
Ciarán <51961306+CiaranMcDonnell@users.noreply.github.com>
2024-01-30 23:27:05 +0000

    Update main.rs
    assists developer functionality

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use rcon::client::RCONClient;
 use rcon::error::RCONError;
 
 fn main() -> Result<(), RCONError> {
-
+    println!("Hello, World!"); // this line helps the developer understand that the programs runs in this line.
+    
     let mut client = RCONClient::new("192.168.1.98:25575", "abc123")?;
 
     client.send_command("Broadcast Hello_Worldddd!");
